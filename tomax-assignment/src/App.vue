a462ba60aacb9468212646da0e9e309f866fc74b
YLROX <yony.limor@roxfinancial.com>
2021-08-09 01:25:45 +0300

    added app layout and devided into components

--- tomax-assignment/src/App.vue
+++ tomax-assignment/src/App.vue
@@ -1,28 +1,109 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <section class="user-images">
+      <ImageEditor class="user-images__editor"></ImageEditor>
+      <div class="user-images__user-list">
+        <User v-for="user in users" :key="user.user" :name="user.name" :code="user.user" :photo="user.picture" />
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImageEditor from './components/ImageEditor.vue'
+import User from './components/User.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ImageEditor,
+    User,
+  },
+  data: () => {
+    return {
+      users: [
+        {
+          "user":1,
+          "name" :"משה",
+          "picture": "https://image.freepik.com/free-icon/user-silhouette_318-79814.jpg"
+        },
+        {
+          "user":2,
+          "name" :"חיים",
+          "picture": "https://thumb1.shutterstock.com/display_pic_with_logo/1442480/564112600/stock-vector-businessman-icon-564112600.jpg"
+        },
+        {
+          "user":3,
+          "name" :"אבי",
+          "picture": "https://thumb1.shutterstock.com/display_pic_with_logo/2239973/365127620/stock-vector-man-icon-365127620.jpg"
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* CSS RESET */
+/* Box sizing rules */
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+/* Remove default padding */
+ul[class],
+ol[class] {
+  padding: 0;
+}
+
+/* Remove default margin */
+body,
+h1,
+h2,
+h3,
+h4,
+p,
+ul[class],
+ol[class],
+li,
+figure,
+figcaption,
+blockquote,
+dl,
+dd {
+  margin: 0;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;500;700&display=swap');
+
+/* Set core body defaults */
+body {
+  min-height: 100vh;
+  scroll-behavior: smooth;
+  text-rendering: optimizeSpeed;
+  line-height: 1.5;
+  font-family: 'Roboto', sans-serif;
+}
+
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.user-images {
+  display: flex;
+  justify-content: space-between;
+}
+
+.user-images__editor {
+  flex: 0 0 600px;
+}
+
+.user-images__user-list {
+  flex: 1;
+  margin-left: 20px;
 }
 </style>
---
+++ tomax-assignment/src/components/ImageEditor.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div ref="imageEditorContainer" class="image-editor">
+      <canvas ref="imageEditor" class="image-editor__canvas"></canvas>
+    </div>
+    <button class="tmx-button">Download Image</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageEditor',
+  mounted() {
+    var canvas = this.$refs.imageEditor,
+        container = this.$refs.imageEditorContainer,
+        ctx = canvas.getContext("2d");
+
+    canvas.width = container.offsetWidth;
+    canvas.height = container.offsetHeight;
+    
+    var background = new Image();
+    background.src = "https://images.pexels.com/photos/1591373/pexels-photo-1591373.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=400&w=600";
+    
+    // Make sure the image is loaded first otherwise nothing will draw.
+    background.onload = function(){
+      ctx.drawImage(background, 0, 0, container.offsetWidth, container.offsetHeight);   
+    }
+  },
+}
+</script>
+
+<style scoped>
+.image-editor {
+  height: 400px;
+  border-radius: 10px;
+  box-shadow: 0 1px 2px rgba(0,0,0, 0.2);
+  overflow: hidden;
+  margin-bottom: 20px;
+  background-color: rgb(223, 223, 223);
+}
+
+.image-editor__canvas {
+  width: 100%;
+  height: 100%;  
+}
+
+.tmx-button {
+  height: 40px;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 20px;
+  background-color: rgb(33, 166, 126);
+  color: #FFF;
+  cursor: pointer;
+}
+</style>
